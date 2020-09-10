64515a33a29cb46c2f5395a4476bffd6ec50b136
sarthak-sriw <Sarthak.btech.ec17@iiitranchi.ac.in>
2020-09-10 20:20:17 +0530

    added test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+  @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "Editing by Sarthak Srivastava";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
